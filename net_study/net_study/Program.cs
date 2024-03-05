110e72f1ebd9b1b248679c819cd09d6a94887944
slrspa14 <kw32482002@gmail.com>
2024-03-05 21:16:42 +0900

    ee

--- net_study/net_study/Program.cs
+++ net_study/net_study/Program.cs
@@ -263,3 +263,512 @@ using System.Threading.Tasks;
 //        }
 //    }
 //}
+//예외처리
+//try, catch, throw
+//namespace Throw
+//{
+//    class Mainapp
+//    {
+//        static void Dosomething(int arg)
+//        {
+//            if (arg < 10)
+//                Console.WriteLine($"arg : {arg}");
+//            else
+//                throw new Exception("arg가 10보다 크네");//throw가 던지는 예외객체 여기서 처리 못해서 호출한놈한테 보내줌
+//        }
+//        static void Main(string[] args)
+//        {
+//            try
+//            {
+//                Dosomething(1);
+//                Dosomething(9);
+//                Dosomething(6);
+//                Dosomething(4);
+//                Dosomething(11);//예외객체를 받고 catch에 던진다
+//                Dosomething(23);
+//            }
+//            catch(Exception e)
+//            {
+//                Console.WriteLine(e.Message);
+//            }
+//        }
+//    }
+//}
+
+//스레드
+using System.Threading;
+//namespace BasicThread
+//{
+//    class Mainapp
+//    {
+//        static void Dosomething()
+//        {
+//            for(int i =0; i< 5; i++)
+//            {
+//                Console.WriteLine($"Thread : {i}");
+//                Thread.Sleep(10);
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            Thread t1 = new Thread(new ThreadStart(Dosomething));//스레드 생성 및 스레드가 실행할 메소드 전달
+//            t1.Start(); //스레드 시작 이제 메모리에 올라가고
+
+//            for(int i=0; i< 5; i++)
+//            {
+//                Console.WriteLine($"Main : {i}");
+//                Thread.Sleep(10);
+//            }
+//            Console.WriteLine("Wating until thread stops...");
+//            t1.Join();//스레드 끝날때까지 메인을 종료시키지 않으려고
+
+//            Console.WriteLine("finished");
+//        }
+//    }
+//}
+// 스레드 임의종료 Abort, interrupt
+//namespace AbortingThrea
+//{
+//    class SideTask
+//    {
+//        int count;
+
+//        public SideTask(int count)
+//        {
+//            this.count = count;
+//        }
+//        public void Keepalive()
+//        {
+//            try
+//            {
+//                while (count > 0)
+//                {
+//                    Console.WriteLine($"{count--} left");
+//                    Thread.Sleep(10);
+//                }
+//                Console.WriteLine("Count : 0");
+//            }
+//            catch (ThreadAbortException a)
+//            {
+//                Console.WriteLine(a); //예외발생시 출력
+//                Thread.ResetAbort();
+//            }
+//            finally
+//            {
+//                Console.WriteLine("clearing resource...");//예외가 발생해도 이건 무조건 실행한다
+//            }
+//        }
+//    }
+//    class Mainapp
+//    {
+//        static void Main(string[] args)
+//        {
+//            SideTask task = new SideTask(100);
+//            Thread t1 = new Thread(new ThreadStart(task.Keepalive));
+//            t1.IsBackground = false;//임계영역 바로 들어가게 하려고
+//            Console.WriteLine("starting thread...");
+//            t1.Start();
+
+//            Thread.Sleep(100);
+
+//            Console.WriteLine("Aborting thread...");
+//            t1.Abort();//스레드 임의종료
+
+//            Console.WriteLine("Wating until thread stops...");
+//            t1.Join();
+
+//            Console.WriteLine("Finished");
+//        }
+//    }
+//}
+//namespace InterruptingThread
+//{
+//    class SideTask
+//    {
+//        int count;
+
+//        public SideTask(int count)
+//        {
+//            this.count = count;
+//        }
+//        public void KeepAlive()
+//        {
+//            try
+//            {
+//                Console.WriteLine("Running thread isn't gonna be interrupted");
+//                Thread.Sleep(100);
+
+//                while (count > 0)
+//                {
+//                    Console.WriteLine($"{count--} left");
+
+//                    Console.WriteLine("Entering into Waitjoinsleep state...");
+//                    Thread.Sleep(10);
+//                }
+//                Console.WriteLine("Count:0");
+//            }
+//            catch (ThreadInterruptedException s)
+//            {
+//                Console.WriteLine(s);
+//            }
+//            finally
+//            {
+//                Console.WriteLine("Clearing resource...");
+//            }
+//        }
+//    }
+//    class Mainapp
+//    {
+//        static void Main(string[] args)
+//        {
+//            SideTask task = new SideTask(100);
+//            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+//            t1.IsBackground = false; //스레드 상태 바꿔주고
+
+//            Console.WriteLine("Starting thread...");
+//            t1.Start();
+
+//            Thread.Sleep(100);
+//            Console.WriteLine("Interrupting thread...");
+//            t1.Interrupt();
+
+//            Console.WriteLine("wating threa stops");
+//            t1.Join();
+//            Console.WriteLine("end");
+//        }
+//    }
+//}
+//동기화
+//lock
+//namespace Synchronize
+//{
+//    class Counter
+//    {
+//        const int LOOP_COUNT = 1000;
+
+//        readonly object thisLock;
+//        private int count;
+//        public int Count
+//        {
+//            get { return count; }//읽기전용 프로퍼티
+//        }
+//        public Counter()
+//        {
+//            thisLock = new object();
+//            count = 0;
+//        }
+
+//        public void Increase()
+//        {
+//            int loopCount = LOOP_COUNT;
+//            while (loopCount-- > 0)
+//            {
+//                //lock(thisLock)
+//                //{
+//                //    count++;
+//                //}
+//                count++;
+//                Thread.Sleep(1);
+//            }
+//        }
+//        public void Decrease()
+//        {
+//            int loopCount = LOOP_COUNT;
+//            while (loopCount-- > 0)
+//            {
+//                //lock(thisLock)
+//                //{
+//                //    count--;
+//                //}
+//                count--;
+//                Thread.Sleep(1);
+//            }
+//        }
+//    }
+//    class Mainapp
+//    {
+//        static void Main(string[] args)
+//        {
+//            Counter counter = new Counter();
+
+//            Thread incthread = new Thread(new ThreadStart(counter.Increase));
+//            Thread decthread = new Thread(new ThreadStart(counter.Decrease));
+
+//            incthread.Start();
+//            decthread.Start();
+
+//            incthread.Join();
+//            decthread.Join();
+//            Console.WriteLine(counter.Count);
+//        }
+//    }
+//}
+//monitor
+//namespace UsingMonitor
+//{
+//    class Counter
+//    {
+//        const int LOOP_COUNT = 1000;
+
+//        readonly object thisLock;
+//        private int count;
+//        public int Count
+//        {
+//            get { return count; }//읽기전용 프로퍼티
+//        }
+//        public Counter()
+//        {
+//            thisLock = new object();
+//            count = 0;
+//        }
+
+//        public void Increase()
+//        {
+//            int loopCount = LOOP_COUNT;
+//            while (loopCount-- > 0)
+//            {
+//                Monitor.Enter(thisLock);
+//                try
+//                {
+//                    count++;
+//                    Console.Write("확");
+//                }
+//                finally
+//                {
+//                    Monitor.Exit(thisLock);
+//                }
+//                Thread.Sleep(1);
+//            }
+//        }
+//        public void Decrease()
+//        {
+//            int loopCount = LOOP_COUNT;
+//            while (loopCount-- > 0)
+//            {
+//                Monitor.Enter(thisLock);
+//                try
+//                {
+//                    count--;
+//                    Console.Write("인");
+//                }
+//                finally
+//                {
+//                    Monitor.Exit(thisLock);
+//                }
+//                Thread.Sleep(1);
+//            }
+//        }
+//    }
+//    class Mainapp
+//    {
+//        static void Main(string[] args)
+//        {
+//            Counter counter = new Counter();
+
+//            Thread incthread = new Thread(new ThreadStart(counter.Increase));
+//            Thread decthread = new Thread(new ThreadStart(counter.Decrease));
+
+//            incthread.Start();
+//            decthread.Start();
+
+//            incthread.Join();
+//            decthread.Join();
+//            Console.WriteLine(counter.Count);
+//        }
+//    }
+//}
+//Monitor.Wit, Monitor.Pulse 저수준 동기화
+namespace WaitPulse
+{
+    class Counter
+    {
+        const int LOOP_COUNT = 1000;
+
+        readonly object thisLock;
+        bool lockedCount = false;//스레드 블록 조건 검사용
+
+        private int count;
+        public int Count
+        {
+            get { return count; }//읽기전용 프로퍼티
+        }
+        public Counter()
+        {
+            thisLock = new object();
+            count = 0;
+        }
+
+        public void Increase()
+        {
+            int loopCount = LOOP_COUNT;
+            while (loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    while (count > 0 || lockedCount == true)//다른 스레드에 의해 true 로 바뀌어있으면 블록
+                    {
+                        Monitor.Wait(thisLock);
+                        Console.WriteLine("ㅌㅅㅌ");
+                    }
+                    lockedCount = true;
+                    count++;
+                    lockedCount = false;//다른 스레드 깨워주고
+                    //Console.Write("확");
+
+                    Monitor.Pulse(thisLock);
+                }
+            }
+        }
+        public void Decrease()
+        {
+            int loopCount = LOOP_COUNT;
+            while (loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    while (count < 0 || lockedCount == true)
+                    {
+                        Monitor.Wait(thisLock);
+                        Console.WriteLine("ㅌㅅㅌ");
+                    }
+                    lockedCount = true;
+                    count--;
+                    lockedCount = false;
+                    //Console.Write("인");
+
+                    Monitor.Pulse(thisLock);
+                }
+            }
+        }
+    }
+    class Mainapp
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            Thread incthread = new Thread(new ThreadStart(counter.Increase));
+            Thread decthread = new Thread(new ThreadStart(counter.Decrease));
+
+            incthread.Start();
+            decthread.Start();
+
+            incthread.Join();
+            decthread.Join();
+            Console.WriteLine(counter.Count);
+        }
+    }
+}
+//using System.IO;
+//namespace UsingTask
+//{
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            string srcFile = args[0];
+//            Action<object> FileCopyAction = (object state) =>
+//            {
+//                string[] paths = (string[])state;
+//                File.Copy(paths[0], paths[1]);
+
+//                Console.WriteLine("TaskId:{0}, ThreadID:{1}, {2} was copied to {3}",
+//                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId, paths[0], paths[1]);
+//            };
+//            Task t1 = new Task(
+//                FileCopyAction,
+//                new string[]{ srcFile, srcFile + ".copy1" });
+//            Task t2 = Task.Run(() =>
+//            {
+//                FileCopyAction(new string[] { srcFile, srcFile + ".copy2" });
+//            });
+//            t1.Start();
+
+//            Task t3 = new Task(
+//                FileCopyAction,
+//                new string[] { srcFile, srcFile + ".copy3" });
+//            t3.RunSynchronously();
+
+//            t1.Wait();
+//            t2.Wait();
+//            t3.Wait();
+//        }
+//    }
+//}
+//Task<TResul>
+//namespace TaskResult
+//{
+//    class Mainapp
+//    {
+//        static bool Isprime(long number)
+//        {
+//            if (number < 2)
+//                return false;
+//            if (number % 2 == 0 && number != 2)
+//                return false;
+//            for(long i = 2; i< number; i++)
+//            {
+//                if (number % i == 0)
+//                    return false;
+//            }
+//            return true;
+//        }
+//        static void Main(string[]args)
+//        {
+//            long from = Convert.ToInt64(args[0]);
+//            long to = Convert.ToInt64(args[1]);
+//            int taskCount = Convert.ToInt32(args[2]);
+
+//            Func<object, List<long>> FindPrimeFunc = 
+//                (objRange) => //반환값있는 Func 대리자 object 매개변수 반환List<long>
+//                {
+//                    long[] range = (long[])objRange;
+//                    List<long> found = new List<long>();//배열 선언
+
+//                    for(long i = range[0]; i<range[1]; i++)
+//                    {
+//                        if (Isprime(i))
+//                            found.Add(i);
+//                    }
+//                    return found;
+
+//                };
+
+//            Task<List<long>>[] tasks = new Task<List<long>>[taskCount];
+//            long currentFrom = from;
+//            long currentTo = to / tasks.Length;
+//            for(int i=0;i<tasks.Length;i++)
+//            {
+//                Console.WriteLine("Task[{0}] : {1} ~ {2}", i, currentFrom, currentTo);
+//                tasks[i] = new Task<List<long>>(FindPrimeFunc, new long[] { currentFrom, currentTo });
+//                currentFrom = currentTo + 1;
+
+//                if (i == tasks.Length - 2)
+//                    currentTo = to;
+//                else
+//                    currentTo = currentTo + (to / tasks.Length);
+//            }
+//            Console.WriteLine("Please press enter to start");
+//            Console.ReadLine();
+//            Console.WriteLine("Started");
+
+//            DateTime startTime = DateTime.Now;
+
+//            foreach (Task<List<long>> task in tasks)
+//                task.Start();
+//            List<long> total = new List<long>();
+
+//            foreach(Task<List<long>> task in tasks)
+//            {
+//                task.Wait();
+//                total.AddRange(task.Result.ToArray());
+//            }
+//            DateTime endTime = DateTime.Now;
+
+//            TimeSpan elapsed = endTime - startTime;
+
+//            Console.WriteLine("Prime number count between {0} and {1} : {2}", from, to, total.Count);
+//            Console.WriteLine("Elapsed time : {0}", elapsed);
+//        }
+//    }
+//}
