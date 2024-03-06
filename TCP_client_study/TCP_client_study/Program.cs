96d9770a6e259f8641c3d08e2e37eda0d347a091
slrspa14 <kw32482002@gmail.com>
2024-03-06 17:09:15 +0900

    server,client

---
+++ TCP_client_study/TCP_client_study/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_client_study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 4)
+            {
+                Console.WriteLine("사용법 :{0} <Bind IP> <Bind Port> <Server IP> <Message>",
+                    Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+            string bindIp = args[0];
+            int bindport = Convert.ToInt32(args[1]);
+            string serverip = args[2];
+            const int serverport = 5425;
+            string message = args[3];
+            try
+            {
+                IPEndPoint clientAddress = new IPEndPoint(IPAddress.Parse(bindIp), bindport);
+                IPEndPoint serveraddress = new IPEndPoint(IPAddress.Parse(serverip), serverport);
+
+                Console.WriteLine("클라이언트 : {0}, 서버 : {1}", clientAddress.ToString(), serveraddress.ToString());
+
+                TcpClient client = new TcpClient(clientAddress);
+
+                client.Connect(serveraddress);
+
+                byte[] data = System.Text.Encoding.Default.GetBytes(message);
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("송신 : {0}", message);
+
+                data = new byte[256];
+
+                string responseData = "";
+
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = Encoding.Default.GetString(data, 0, bytes);
+                Console.WriteLine("수신 : {0}", responseData);
+
+                stream.Close();
+                client.Close();
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("클라이언트 종료");
+        }
+    }
+}
---
+++ TCP_server_study/TCP_server_study/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_server_study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("사용법 :{0} <Bind IP>", Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+            string bindIP = args[0];//ip
+            const int bindPort = 5425;//포트번호 상수
+            TcpListener server = null;//서버 객체 생성
+            try
+            {
+                IPEndPoint localAddress = new IPEndPoint(IPAddress.Parse(bindIP), bindPort);// ip,port할당
+                server = new TcpListener(localAddress);
+                server.Start();//서버시작
+
+                //accept
+                Console.WriteLine("에코서버 시작");
+                while(true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("클라이언트 접속 : {0}", ((IPEndPoint)client.Client.RemoteEndPoint).ToString());
+
+                    NetworkStream stream = client.GetStream();
+
+                    int length;
+                    string data = null;
+                    byte[] bytes = new byte[256];
+                    while((length = stream.Read(bytes, 0, bytes.Length))!=0)//받은 데이터, , 데이터 길이
+                    {
+                        data = Encoding.Default.GetString(bytes, 0, length);//인코딩
+                        Console.WriteLine("수신 : {0}", data);
+
+                        byte[] msg = Encoding.Default.GetBytes(data); //에코서버라서 받은거 바로 보내주려고 담고
+
+                        stream.Write(msg, 0, msg.Length);//보낼 데이터, , 데이터 길이전송
+                        Console.WriteLine("송신 : {0}", data);
+                    }
+                    //연결종료
+                    stream.Close();
+                    client.Close();
+                }
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                server.Stop();//연결요청 수신대기 종료
+            }
+            Console.WriteLine("섭종");
+        }
+    }
+}
